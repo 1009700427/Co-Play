49b71d091d86b73bfc9553fbdd5d5bca7bf2a90c
Steven Xu <siyuanx@usc.edu>
2018-07-08 20:55:29 +0800

    config files

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+/**
+ * Created by siyuanxu on 2018/6/6.
+ */
+
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './reactApp/app.js',
+    output: {
+        path: __dirname + '/reactApp/build',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /reactApp\/(.)*\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.(s?css|less)$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+            {
+                test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+                use: [
+                    {
+                        loader: 'url-loader'
+                    },
+                ]
+            },
+            {
+                test: /\.(eot|otf|svg|ttf|woff|woff2)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        hot: true,
+        inline: true,
+        open: true,
+        hotOnly: true,
+        historyApiFallback: true
+    },
+    devtool: "inline-sourcemap"
+};
